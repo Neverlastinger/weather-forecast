eadf4cd4fa2f72bc58f20c8472448358dee8be67
Radoslav Popov <you@example.com>
2018-11-13 20:12:12 +0200

    Initial layout of the application with static data

--- src/js/App.js
+++ src/js/App.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
+	render() {
+		return (
+			<div className="container">
 
-      </div>
-    );
-  }
+				<div class="input">
+					<input type="text" placeholder="Sofia,bg / London,uk / Singapore,sg" />
+				</div>
+
+				<ul>
+					<li>1</li>
+					<li>2</li>
+					<li>3</li>
+					<li>4</li>
+					<li>5</li>
+				</ul>
+			</div>
+		);
+	}
 }
 
 export default App;
