521d3c6807241f88642e668b18bf16b7e9ace9fc
Your Name <you@example.com>
2018-11-13 19:38:17 +0200

    Added the build/ folder in order to set up GitHub Pages

---
+++ build/precache-manifest.7e5c24abbb8efdeae3ac5205ad839378.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0027c5b63a7f2dfb1b37071d3a765257",
+    "url": "/static/media/background.0027c5b6.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5e2890645706ee4412bf",
+    "url": "/static/js/main.5e289064.chunk.js"
+  },
+  {
+    "revision": "f68dcc1a0c0c6e129be0",
+    "url": "/static/js/1.f68dcc1a.chunk.js"
+  },
+  {
+    "revision": "5e2890645706ee4412bf",
+    "url": "/static/css/main.494c87db.chunk.css"
+  },
+  {
+    "revision": "b6d9850c0ba2f37e6140bc86ddb589b4",
+    "url": "/index.html"
+  }
+];
