0b8e69eb1d7171bf14c2aab9d945d01ab703d02d
Radoslav Popov <rpopov@hostopia.com>
2018-11-14 15:46:47 +0200

    Added the latest build

---
+++ build/precache-manifest.c2c49d9394f72003ca9a49da678170bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "14c48bd10d3551cc39b8",
+    "url": "/static/css/main.5b510612.chunk.css"
+  },
+  {
+    "revision": "14c48bd10d3551cc39b8",
+    "url": "/static/js/main.14c48bd1.chunk.js"
+  },
+  {
+    "revision": "ee369e1a75938fbc5ab9",
+    "url": "/static/js/1.ee369e1a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "50613f7c2a24bbae4aec210328dc68f7",
+    "url": "/index.html"
+  }
+];
